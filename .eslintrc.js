28c50c6d8746607d86ac2d18cca759b5d9d571da
gennadiySinigur <gennadiysinigur@gmail.com>
2022-08-08 20:58:51 +0300

    Configured some Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,9 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
   ],
+  overrides: [
+  ],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
@@ -18,5 +17,16 @@ module.exports = {
     'react',
   ],
   rules: {
+    'react/prefer-stateless-function': [0],
+    'no-underscore-dangle': 'off',
+    'react/jsx-filename-extension': [0],
+    'react/destructuring-assignment': [0],
+    'class-methods-use-this': 'off',
+    'react/function-component-definition': [0],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/click-events-have-key-events': [0],
+    'react/jsx-curly-spacing': [2, { when: 'never', allowMultiline: false }],
+    'operator-linebreak': ['error', 'after'],
+    'react/state-in-constructor': ['error', 'never'],
   },
 };
