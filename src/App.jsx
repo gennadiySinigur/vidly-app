cd1281b97d10f04b2f799f391440f2c7ab5b1c1f
gennadiySinigur <gennadiysinigur@gmail.com>
2022-10-06 20:17:56 +0300

    Added space after heading

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1>Vidly</h1>
+        <h1 className="mb-3">Vidly</h1>
 
         <Movies />
       </main>
